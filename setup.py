9235fbcd33e5eeaaf0aef877cdec5460cf18f898
joel.kleier <joel@kleier.us>
2017-01-03 13:57:58 -0600

    update setup.py meta info

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+# This file is part of Reliquary.
+#
+# Reliquary is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 
 from setuptools import setup, find_packages
@@ -31,10 +46,14 @@ setup(name='reliquary',
       description='reliquary',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Environment :: Web Environment",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
           "Programming Language :: Python",
-          "Framework :: Pyramid",
+          "Programming Language :: Python :: 3.5",
+          "Topic :: Internet :: Proxy Servers",
           "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
       ],
       author='Joel Kleier',
       author_email='joel@kleier.us',
@@ -47,11 +66,12 @@ setup(name='reliquary',
           'testing': tests_require,
       },
       install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = reliquary:main
-      [console_scripts]
-      init_reliquary = reliquary.scripts:init_reliquary
-      reindex_reliquary = reliquary.scripts:reindex
-      """,
-      )
+      entry_points={
+          "paste.app_factory": [
+              "main = reliquary:main"
+          ],
+          "console_scripts": [
+              "init_reliquary = reliquary.scripts:init_reliquary",
+              "reindex_reliquary = reliquary.scripts:reindex",
+          ],
+      })
